536f8a1713b78920527aa95503a7f3449b1ad689
Luis Rafael Diaz Anaya <53496639+luisdico313@users.noreply.github.com>
2025-07-19 22:56:05 -0500

    Creación Inicial

---
+++ Pacientes/src/main/java/PaquetePacientes/Pacientes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package PaquetePacientes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Pacientes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
